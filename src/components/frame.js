dd21664f79960c603a32df51b3ed4f4993a54af5
JenabB <birulangit1996@gmail.com>
2021-08-21 08:03:34 +0700

    finished part 2

---
+++ src/components/frame.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const frame = (children) => {
+  return (
+    <div>
+      <h1>This is the page title</h1>
+      {children}
+    </div>
+  );
+};
+
+export default frame;
